792dad7f0359514de544ae9205e0d25b2d16760a
4t0n6 <coreacezone@gmail.com>
2023-07-15 20:45:59 +0800

    sample: use composable

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useCapitalizeText } from '@/composables/capitalize-text'
+
+const { text, capitalizeText } = useCapitalizeText()
+</script>
+
+<template>
+    <div v-if="capitalizeText">
+        <p>{{ capitalizeText }}</p>
+    </div>
+    <div>
+        <input type="text" v-model="text" />
+    </div>
+</template>
