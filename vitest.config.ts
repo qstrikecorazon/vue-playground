da3ea797845df2dc816dbe860bcea751b5617a3b
4t0n6 <coreacezone@gmail.com>
2023-07-15 20:44:58 +0800

    fix vitest type error and remove generated templates.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,20 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig } from 'vite'
-import { configDefaults, defineConfig } from 'vitest/config'
+import { type UserConfig, configDefaults, defineConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-      transformMode: {
-        web: [/\.[jt]sx$/]
-      }
+const vitestConfig = defineConfig({
+  test: {
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    root: fileURLToPath(new URL('./', import.meta.url)),
+    transformMode: {
+      web: [/\.[jt]sx$/]
     }
-  })
+  }
+})
+
+export default mergeConfig(
+  viteConfig as UserConfig,
+  vitestConfig as UserConfig
 )
