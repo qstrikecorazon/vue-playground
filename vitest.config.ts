dad849a3fc5d659d14a12cbcc65a7704dcff9590
qstrikecorazon <102020291+qstrikecorazon@users.noreply.github.com>
2023-07-16 01:22:39 +0800

    Revert "Playground"

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,20 +1,18 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig } from 'vite'
-import { type UserConfig, configDefaults, defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
-const vitestConfig = defineConfig({
-  test: {
-    environment: 'jsdom',
-    exclude: [...configDefaults.exclude, 'e2e/*'],
-    root: fileURLToPath(new URL('./', import.meta.url)),
-    transformMode: {
-      web: [/\.[jt]sx$/]
-    }
-  }
-})
-
 export default mergeConfig(
-  viteConfig as UserConfig,
-  vitestConfig as UserConfig
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      transformMode: {
+        web: [/\.[jt]sx$/]
+      }
+    }
+  })
 )
