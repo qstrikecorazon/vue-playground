ec454f8dec4fd0c82324dadc93a28addb1279de9
qstrikecorazon <corazon@qstrike.com>
2023-07-16 16:27:41 +0800

    sample composable text

---
+++ src/composables/paragraph.ts
@@ -0,0 +1,15 @@
+import { ref } from 'vue'
+
+const txt = ref('')
+let hasDefault = false
+
+export function useParagraph(defaultText: string = '') {
+    function setText(text: string) {
+        txt.value = text
+    }
+    
+    if (!hasDefault && defaultText) setText(defaultText)
+    if (!hasDefault) hasDefault = true
+
+    return { txt, setText }
+}
---
+++ src/views/ParagraphView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Paragraph from '@/components/Paragraph.vue'
+import { useParagraph } from '@/composables/paragraph'
+import { useRoute } from 'vue-router'
+
+const route = useRoute();
+const { txt, setText } = useParagraph(
+    typeof route.query['text'] === 'string' ? route.query['text']: ''
+)
+
+type onInputCallback = (e: Event) => void
+
+const onInput: onInputCallback = e => {
+    setText((e.target as HTMLInputElement).value)
+}
+
+</script>
+
+<template>
+    <Paragraph />
+    <Paragraph />
+    <Paragraph />
+    <Paragraph />
+    <Paragraph />
+    <input type="text" @input="onInput" :value="txt">
+</template>
