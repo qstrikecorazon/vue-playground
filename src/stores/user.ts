7198279b02433a7a026e773627ed4ad24aa8cd56
qstrikecorazon <corazon@qstrike.com>
2023-07-16 10:33:10 +0800

    add type to user

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,8 +1,15 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
+interface User {
+    id: number;
+    name: string;
+    age: number;
+    is_admin: 0 | 1
+}
+
 export const useUserStore = defineStore('user', () => {
-    const user = ref({
+    const user = ref<User | undefined>({
         id: 1,
         name: 'corazon',
         age: 24,
@@ -10,7 +17,7 @@ export const useUserStore = defineStore('user', () => {
     })
 
     function isAdmin(): boolean {
-        return Boolean(user.value.is_admin)
+        return Boolean(user.value?.is_admin)
     }
 
     return { user, isAdmin };
