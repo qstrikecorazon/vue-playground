82284d799b31d50cff362ef4d394b3e54e8e4a4e
qstrikecorazon <corazon@qstrike.com>
2023-07-16 16:23:15 +0800

    simple array mutation pinia.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { useCapitalizeText } from '@/composables/capitalize-text'
 import { useUserStore } from '@/stores/user'
+import { useItemsStore } from '@/stores/items'
 
 const { text, capitalizeText } = useCapitalizeText()
 const userStore = useUserStore();
+const itemsStore = useItemsStore();
 
 onMounted(() => {
     setTimeout(() => {
@@ -29,5 +31,22 @@ onMounted(() => {
     </div>
     <div>
         <input type="text" v-model="text" />
+    </div>
+    <div>
+        <button @click="userStore.$reset()">click me</button>
+    </div>
+
+    <div v-if="itemsStore.items.length">
+        <h4>Unix commands:</h4>
+        <div v-for="(item, i) in itemsStore.items" :key="i">
+            {{ item }} <span><button @click="itemsStore.removeItem(item)">x</button></span>
+        </div>
+    </div>
+    <div v-else>
+        <button @click="itemsStore.$reset()">reset</button>
+    </div>
+
+    <div>
+        First: {{ itemsStore.getUppercasedFirstItem }}
     </div>
 </template>
